e041f6f639369088a04f7cc4fca2f1849fe725ad
junilshres <junilshr@gmail.com>
2025-08-03 15:56:59 -0400

    test only

--- tests/codegen.spec.js
+++ tests/codegen.spec.js
@@ -9,4 +9,5 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('banner').getByRole('img', { name: 'profile picture' }).click();
   await page.getByRole('menuitem', { name: 'Logout' }).click();
+  
 });
